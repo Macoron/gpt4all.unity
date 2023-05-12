fb812991ea8f9de5198d8f1f73bafbc4dc43fd40
Alexander Evgrashin <aevgrashin@yandex.ru>
2023-05-12 22:43:58 +0200

    Build script changes

--- build_cpp.bat
+++ build_cpp.bat
@@ -6,4 +6,5 @@ cmake -B ./build -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release .
 
 cd ./build
 cmake --build . --parallel
-xcopy /y /q .\bin\Release\whisper.dll %MY_PATH%\Packages\com.whisper.unity\Plugins\Windows\libwhisper.dll*
+xcopy /y /q .\libllmodel.dll %MY_PATH%\Packages\com.gpt4all.unity\Plugins\Windows\libllmodel.dll*
+xcopy /y /q .\bin\libllama.dll %MY_PATH%\Packages\com.gpt4all.unity\Plugins\Windows\libllama.dll*
--- build_cpp.sh
+++ build_cpp.sh
@@ -2,7 +2,6 @@
 
 gpt4all_path="$1"
 targets=${2:-all}
-android_sdk_path="$3"
 unity_project="$PWD"
 build_path="$gpt4all_path/llmodel/build"
 
@@ -28,50 +27,10 @@ build_mac() {
   echo "Build files copied to $target_path"
 }
 
-build_ios() {
-  clean_build
-  echo "Starting building for ios..."
-
-  cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_SYSTEM_NAME=iOS \
-  "-DCMAKE_OSX_ARCHITECTURES=arm64;x86_64" \
-   -DCMAKE_SYSTEM_PROCESSOR=arm64 -DCMAKE_IOS_INSTALL_COMBINED=YES ../
-  make
-
-  echo "Build for ios complete!"
-
-  artifact_path="$build_path/libwhisper.a"
-  target_path="$unity_project/Packages/com.whisper.unity/Plugins/iOS/libwhisper.a"
-  cp "$artifact_path" "$target_path"
-
-  echo "Build files copied to $target_path"
-}
-
-build_android() {
-  clean_build
-  echo "Starting building for Android..."
-
-  cmake -DCMAKE_TOOLCHAIN_FILE="$android_sdk_path" -DANDROID_ABI=arm64-v8a -DBUILD_SHARED_LIBS=OFF ../
-  make
-
-  echo "Build for Android complete!"
-
-  artifact_path="$build_path/libwhisper.a"
-  target_path="$unity_project/Packages/com.whisper.unity/Plugins/Android/libwhisper.a"
-  cp "$artifact_path" "$target_path"
-
-  echo "Build files copied to $target_path"
-}
-
 if [ "$targets" = "all" ]; then
   build_mac
-  build_ios
-  build_android
 elif [ "$targets" = "mac" ]; then
   build_mac
-elif [ "$targets" = "ios" ]; then
-  build_ios
-elif [ "$targets" = "android" ]; then
-  build_android
 else
   echo "Unknown targets: $targets"
 fi
